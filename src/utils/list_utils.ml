40058a88a55ad6212c15594c1a5d7a8ee0ea58a8
Filip Koprivec <koprivec.filip@gmail.com>
2020-12-03 09:44:17 +0100

    Day 1 solutions

--- src/utils/list_utils.ml
+++ src/utils/list_utils.ml
@@ -10,4 +10,13 @@ module List = struct
   let lines = String.split_on_char '\n'
 
   let with_index l = List.mapi (fun i x -> (i, x)) l
+
+  let count a l = l |> List.filter (fun x -> x = a) |> List.length
+
+  let list_of_string s = s |> String.to_seq |> List.of_seq
+
+  let count_filter f l = l |> List.filter f |> List.length
+
+  let count_filter2 f l =
+    List.fold_right (fun x s -> match f x with true -> s + 1 | false -> s) l 0
 end
