8f8a7e70419d76f11c741637da7086f3a99bc6d9
Filip Koprivec <koprivec.filip@gmail.com>
2020-12-15 16:50:40 +0100

    Day 13

--- src/solutions/solution.ml
+++ src/solutions/solution.ml
@@ -17,6 +17,7 @@ let choose_solver : string -> (module Solver) = function
   | "12" -> (module Day12.Solver)
   | "13" -> (module Day13.Solver)
   | "14" -> (module Day14.Solver)
+  | "15" -> (module Day15.Solver)
   | _ -> failwith "Ni še rešeno"
 
 let main () =
