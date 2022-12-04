809338907fe4f46b9855c52c07926167c50486c8
pg008 <gabrijel.plum@gmail.com>
2022-12-04 09:59:06 +0100

    četrti dan

--- src/solvers/year_2022/day4.ml
+++ src/solvers/year_2022/day4.ml
@@ -1,9 +1,37 @@
 open Solvers.Signature
-
-(* open Utils.List_utils *)
+open Utils.List_utils
 
 module Solver : Solver = struct
-  let naloga1 data = data
+  type obmocje = { zacetek : int; konec : int }
 
-  let naloga2 data _part1 = data
+  let obseg_obmocja o = o.konec - o.zacetek + 1
+
+  let rec vsebuje_drugo_obmocje o1 o2 =
+    if obseg_obmocja o1 >= obseg_obmocja o2 then
+      o1.zacetek <= o2.zacetek && o2.konec <= o1.konec
+    else vsebuje_drugo_obmocje o2 o1
+
+  let rec se_obmocji_prekrivata o1 o2 =
+    if o1.zacetek <= o2.zacetek then o2.zacetek <= o1.konec
+    else se_obmocji_prekrivata o2 o1
+
+  let obmocje_iz_niza n =
+    match String.split_on_char '-' n |> List.map int_of_string with
+    | [ z; k ] -> { zacetek = z; konec = k }
+    | _ -> failwith ("Neprimeren format območja: " ^ n ^ ".")
+
+  let obmocji_iz_niza n =
+    match String.split_on_char ',' n |> List.map obmocje_iz_niza with
+    | [ o1; o2 ] -> (o1, o2)
+    | _ -> failwith ("Neprimerno število območij" ^ n ^ ".")
+
+  let naloga1 data =
+    List.lines data |> List.map obmocji_iz_niza
+    |> List.map (fun (o1, o2) -> vsebuje_drugo_obmocje o1 o2)
+    |> List.count true |> string_of_int
+
+  let naloga2 data _part1 =
+    List.lines data |> List.map obmocji_iz_niza
+    |> List.map (fun (o1, o2) -> se_obmocji_prekrivata o1 o2)
+    |> List.count true |> string_of_int
 end
