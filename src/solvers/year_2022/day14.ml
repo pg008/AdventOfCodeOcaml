9923bd1c54204dcf280eda0a4cf325e76517d8f8
pg008 <gabrijel.plum@gmail.com>
2022-12-16 15:57:03 +0100

    štirinajsti dan

--- src/solvers/year_2022/day14.ml
+++ src/solvers/year_2022/day14.ml
@@ -66,8 +66,15 @@ module Solver : Solver = struct
   type poligon = {
     zgoraj_levo : int * int;
     polja : polnilo Array.t Array.t;
-    mutable vrh_kupcka : koordinata option;
+    izvir : koordinata;
   }
+
+  let prazen_poligon zgoraj_levo sirina visina polnilo =
+    {
+      zgoraj_levo;
+      polja = Array.make_matrix visina sirina polnilo;
+      izvir = (500, 0);
+    }
 
   let indeks_iz_koordinate zgoraj_levo k =
     let j, i = k -: zgoraj_levo in
@@ -77,6 +84,10 @@ module Solver : Solver = struct
     let i, j = indeks_iz_koordinate poligon.zgoraj_levo koordinata in
     poligon.polja.(i).(j)
 
+  let pridobi_opt poligon koordinata =
+    let i, j = indeks_iz_koordinate poligon.zgoraj_levo koordinata in
+    try Some poligon.polja.(i).(j) with Invalid_argument _ -> None
+
   let nastavi poligon koordinata vrednost =
     let i, j = indeks_iz_koordinate poligon.zgoraj_levo koordinata in
     poligon.polja.(i).(j) <- vrednost
@@ -85,29 +96,15 @@ module Solver : Solver = struct
     let linije =
       String.split_on_char '\n' niz |> List.map preberi_linije |> List.concat
     in
-    let (x0, y0), (x1, y1) = min_max_koordinate linije in
+    let (x0, _), (x1, y1) = min_max_koordinate linije in
     let y0' = 0 and y1' = y1 + 2 in
-    let x0' = min x0 (500 - y1' - 10) and x1' = max x1 (500 + y1' + 10) in
+    let x0' = min x0 (500 - y1') and x1' = max x1 (500 + y1') in
     let sirina = x1' - x0' + 1
     and visina = y1' + 1
     and koordinate_skal = linije |> List.map koordinate_linije |> List.concat
     and koordinate_tal = koordinate_linije ((x0', y1'), (x1', y1')) in
-    let poligon =
-      {
-        zgoraj_levo = (x0', y0');
-        polja = Array.make_matrix visina sirina Zrak;
-        vrh_kupcka = None;
-      }
-    in
-    List.iter
-      (fun k ->
-        nastavi poligon k Skala;
-        if
-          fst k = 500
-          && ( poligon.vrh_kupcka = None
-             || snd (Option.get poligon.vrh_kupcka) > snd k )
-        then poligon.vrh_kupcka <- Some (500, snd k))
-      koordinate_skal;
+    let poligon = prazen_poligon (x0', y0') sirina visina Zrak in
+    List.iter (fun k -> nastavi poligon k Skala) koordinate_skal;
     List.iter (fun k -> nastavi poligon k Tla) koordinate_tal;
     poligon
 
@@ -117,83 +114,47 @@ module Solver : Solver = struct
     in
     Array.map niz_vrstice poligon.polja |> Array.to_list |> String.concat "\n"
 
-  let rec simuliraj_padec poligon (polozaj : koordinata) : unit =
-    if polozaj = (500, 0) then
-      simuliraj_padec poligon (Option.get poligon.vrh_kupcka -: (0, 1))
-    else
-      let kl, k0, kd =
-        (polozaj +: (-1, 1), polozaj +: (0, 1), polozaj +: (1, 1))
-      in
-      try
-        match pridobi poligon k0 with
-        | Zrak -> simuliraj_padec poligon k0
-        | Tla -> raise (Invalid_argument "")
-        | Skala | Pesek -> (
-            match pridobi poligon kl with
-            | Zrak -> simuliraj_padec poligon kl
-            | Tla -> raise (Invalid_argument "")
-            | Skala | Pesek -> (
-                match pridobi poligon kd with
-                | Zrak -> simuliraj_padec poligon kd
-                | Tla -> raise (Invalid_argument "")
-                | Skala | Pesek ->
-                    nastavi poligon polozaj Pesek;
-                    if
-                      fst polozaj = 500
-                      && snd polozaj < snd (Option.get poligon.vrh_kupcka)
-                    then poligon.vrh_kupcka <- Some polozaj ) )
-      with Invalid_argument _ -> raise PadecVNeskoncnost
+  let naslednji_polozaj dovoli_tla poligon (trenutni_polozaj : koordinata) :
+      koordinata option =
+    let l_k = trenutni_polozaj +: (-1, 1)
+    and s_k = trenutni_polozaj +: (0, 1)
+    and d_k = trenutni_polozaj +: (1, 1) in
+    let l = pridobi_opt poligon l_k
+    and s = pridobi_opt poligon s_k
+    and d = pridobi_opt poligon d_k in
+    match (l, s, d) with
+    | _, Some Zrak, _ -> Some s_k
+    | _, None, _ -> None
+    | _, Some Tla, _ when not dovoli_tla -> None
+    | Some Zrak, _, _ -> Some l_k
+    | None, _, _ -> None
+    | Some Tla, _, _ when not dovoli_tla -> None
+    | _, _, Some Zrak -> Some d_k
+    | _, _, None -> None
+    | _, _, Some Tla when not dovoli_tla -> None
+    | _ -> Some trenutni_polozaj
 
-  let rec simuliraj_padec_s_tlemi poligon (polozaj : koordinata) : unit =
-    if polozaj = (500, 0) then
-      if pridobi poligon polozaj = Pesek then raise ZapolnjenIzvir
-      else
-        simuliraj_padec_s_tlemi poligon (Option.get poligon.vrh_kupcka -: (0, 1))
-    else
-      let kl, k0, kd =
-        (polozaj +: (-1, 1), polozaj +: (0, 1), polozaj +: (1, 1))
-      in
-      try
-        match pridobi poligon k0 with
-        | Zrak -> simuliraj_padec_s_tlemi poligon k0
-        | Skala | Pesek | Tla -> (
-            match pridobi poligon kl with
-            | Zrak -> simuliraj_padec_s_tlemi poligon kl
-            | Skala | Pesek | Tla -> (
-                match pridobi poligon kd with
-                | Zrak -> simuliraj_padec_s_tlemi poligon kd
-                | Skala | Pesek | Tla ->
-                    nastavi poligon polozaj Pesek;
-                    if
-                      fst polozaj = 500
-                      && snd polozaj < snd (Option.get poligon.vrh_kupcka)
-                    then poligon.vrh_kupcka <- Some polozaj ) )
-      with Invalid_argument _ -> raise PadecVNeskoncnost
+  let rec simuliraj_padec dovoli_tla poligon (polozaj : koordinata) : unit =
+    if pridobi poligon poligon.izvir = Pesek then raise ZapolnjenIzvir;
+    let polozaj' = naslednji_polozaj dovoli_tla poligon polozaj in
+    match polozaj' with
+    | Some p when p = polozaj -> nastavi poligon polozaj Pesek
+    | Some p -> simuliraj_padec dovoli_tla poligon p
+    | None -> raise PadecVNeskoncnost
 
-  let prestej_padce poligon : int =
+  let prestej_padce dovoli_tla poligon : int =
     let nov_poligon = { poligon with polja = Array.(map copy) poligon.polja } in
     let rec aux st_padcev =
       try
-        simuliraj_padec nov_poligon (500, 0);
+        simuliraj_padec dovoli_tla nov_poligon nov_poligon.izvir;
         aux (st_padcev + 1)
-      with PadecVNeskoncnost -> st_padcev
+      with PadecVNeskoncnost | ZapolnjenIzvir -> st_padcev
     in
     aux 0
 
-  let prestej_padce2 poligon : int =
-    let nov_poligon = { poligon with polja = Array.(map copy) poligon.polja } in
-    let rec aux st_padcev =
-      try
-        simuliraj_padec_s_tlemi nov_poligon (500, 0);
-        aux (st_padcev + 1)
-      with
-      | ZapolnjenIzvir -> st_padcev
-      | PadecVNeskoncnost -> failwith "zmanjkalo mi je tal"
-    in
-    aux 0
-
-  let naloga1 data = preberi_poligon data |> prestej_padce |> string_of_int
+  let naloga1 data =
+    preberi_poligon data |> prestej_padce false |> string_of_int
 
   let naloga2 data _part1 =
-    preberi_poligon data |> prestej_padce2 |> string_of_int
+    preberi_poligon data |> prestej_padce true |> string_of_int
 end
